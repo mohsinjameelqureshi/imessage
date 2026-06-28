99c588778f5317591018974aaeb0717733e2a5c3
Mohsin Jameel <qureshimohsinjameel@gmail.com>
2026-06-28 23:20:39 +0500

    Establishes core client-side infrastructure
    Integrates Zustand for global state management, particularly for syncing authentication status and performing backend verification. Adds Axios for API communication and Socket.io client for real-time capabilities.
    
    Incorporates React Hot Toast to provide global user feedback through notifications. Enhances the initial application loading sequence to account for asynchronous backend authentication checks.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,11 +5,25 @@ import ChatPage from "./pages/ChatPage.jsx";
 import AuthPage from "./pages/AuthPage.jsx";
 import { useAuth } from "@clerk/react";
 import PageLoader from "./components/PageLoader.jsx";
+import { useAuthStore } from "./store/useAuthStore.js";
+import { useEffect } from "react";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   const { isSignedIn, isLoaded } = useAuth();
 
-  if (!isLoaded) return <PageLoader />;
+  const checkAuth = useAuthStore((state) => state.checkAuth);
+  const clearAuth = useAuthStore((state) => state.clearAuth);
+  const isCheckingAuth = useAuthStore((state) => state.isCheckingAuth);
+
+  useEffect(() => {
+    if (!isLoaded) return;
+
+    if (isSignedIn) checkAuth();
+    else clearAuth();
+  }, [checkAuth, clearAuth, isLoaded, isSignedIn]);
+
+  if (!isLoaded || (isSignedIn && isCheckingAuth)) return <PageLoader />;
 
   return (
     <ThemeProvider>
@@ -26,6 +40,7 @@ function App() {
             element={!isSignedIn ? <AuthPage /> : <Navigate to={"/"} replace />}
           />
         </Routes>
+        <Toaster />
       </WallpaperProvider>
     </ThemeProvider>
   );
